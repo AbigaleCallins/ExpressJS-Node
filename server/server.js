0a3a09a7351ffdaa63e8b97d07851fb7e2dd7d9a
AbigaleCallins <alcallins@gmail.com>
2022-03-25 10:57:10 -0500

    saving

--- server/server.js
+++ server/server.js
@@ -1,31 +1,33 @@
 const express = require('express');
-const path = require('path');
-// const fs = require('fs');
+// const path = require('path');
+
 
 const app = express();
 
-// server that responds to the root get request
-app.get("/", (req, res) => {
 
-    res.send("Hello from the web server side...")
+// app.get("/", (req, res) => {
 
-});
+//     res.send("Hello from the web server side...")
 
-// Use express.static to serve files from a folder named public in the root of your project.
+
+// });
+
 
 app.use((req, res, next) => {
     console.log(req.url);
     next();
 })
+app.use('/request-type', (req, res, next) => {
+    console.log('Request type: ', req.method);
+    next();
+  });
+  app.get('/', (req, res) => {
+    res.send('Successful klresponse.');
+  });
 
-app.use(express.static(path.join(__dirname, "../public")));
-
-// app.get('/', (req, res) => {
-//   res.send('"Hello from the web server side...".');
-// });
+// app.use(express.static(path.join(__dirname, "../public/index.html")));
 
 
-
-app.use('/express', path('public'));
+// app.use('/express', path('public'));
 
 app.listen(3000);
