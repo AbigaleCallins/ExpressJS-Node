5a3c8cb0e365a941f3f7d39f81d2ae8f54a85c5e
AbigaleCallins <alcallins@gmail.com>
2022-03-25 16:48:10 -0500

    'add'

--- server/server.js
+++ server/server.js
@@ -1,33 +1,46 @@
 const express = require('express');
-// const path = require('path');
-
+const path = require('path');
+// const bodyParser = require('body-parser');
+const fs = require('fs')
 
 const app = express();
 
+/* Create your own middleware using app.use that console.logs every 
+req.url and passes flow to the next function */
+app.use(express.urlencoded({ extended: false }));
 
+app.post('/contact-form', (req,res, next) =>{
+  console.log('Request type: ', req.method);
+
+  console.log(req.body.email);
+  console.log(req.body.name);
+  res.send('Hello');
+  next()
+});
+
+
+// // Expresss server , with root request
 // app.get("/", (req, res) => {
-
 //     res.send("Hello from the web server side...")
-
-
 // });
 
+// app.use((req, res, next) => {
+//     console.log(req.url);
+//     next();
+// })
+// app.use('/contact-form', (req, res, next) => {
+//     console.log('Request type: ', req.method);
+//     next();
+//   });
+//   app.get('/', (req, res) => {
+//     res.send('success');
+//   });
 
-app.use((req, res, next) => {
-    console.log(req.url);
-    next();
-})
-app.use('/request-type', (req, res, next) => {
-    console.log('Request type: ', req.method);
-    next();
-  });
-  app.get('/', (req, res) => {
-    res.send('Successful klresponse.');
-  });
+//   app.get("/formsubmissions", (req, res) => {
+//     fs.readFile("formsubmissions.json", (err, data) => res.send(data));
+// });
 
-// app.use(express.static(path.join(__dirname, "../public/index.html")));
-
-
+app.use(express.static(path.join(__dirname, "../public")));
 // app.use('/express', path('public'));
 
 app.listen(3000);
